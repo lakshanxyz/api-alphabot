93ef5d53b9032ed089c0dcb57429f42fd82a815d
itsuki-chan <82200440+Adiixyz@users.noreply.github.com>
2021-09-07 14:27:39 +0800

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -24,10 +24,10 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            namabot: 'ItsukiBot',
+            namaowner: 'Itsuki/Adiixyz',
+            instagram: 'adiistah',
+            youtube : 'Adiixyz x 404'
         }
     }
     res.json(config)
